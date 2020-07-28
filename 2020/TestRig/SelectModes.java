be7d7120c03aedc7c032d279008963539b284c8b
h0llycarter <hollycarter@rocknrollrobots25.com>
2020-07-27 20:15:33 -0700

    implemented selectModes class and initial implementation for testrig demo

--- 2020/TestRig/SelectModes.java
+++ 2020/TestRig/SelectModes.java
@@ -1,5 +1,9 @@
 package TestRig;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import team25core.Robot;
+
 public class SelectModes {
     public enum TestRigMode {
         INDEP_MOTOR,
@@ -16,5 +20,17 @@ public class SelectModes {
                 return values()[0];
             }
         }
+    }
+
+    private Robot myRobot;
+    private TestRigMode mode;
+    private Telemetry.Item msgTelem;
+    private Telemetry.Item modeTelem;
+
+    SelectModes(Robot theirRobot) {
+        myRobot = theirRobot;
+        mode = TestRigMode.INDEP_MOTOR;
+        msgTelem = myRobot.telemetry.addData("Use right trigger to toggle through modes");
+        modeTelem = myRobot.telemetry.addData("Current mode: " , mode);
     }
 }
--- 2020/TestRig/TestRigDemo.java
+++ 2020/TestRig/TestRigDemo.java
@@ -2,11 +2,49 @@ package opmodes.Regionals;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import TestRig.SelectModes;
+import team25core.GamepadTask;
+import team25core.Robot;
+import team25core.RobotEvent;
 import team25core.StandardFourMotorRobot;
 
 @TeleOp(name = "Test Rig Demo")
 //@Disabled
 
-public class TestRigDemo extends StandardFourMotorRobot {
+public class TestRigDemo extends Robot {
+    private GamepadTask gamepad1Task;
+    private SelectModes modes;
 
+    // this init method is called when the init button is pressed on the driver station
+    @Override
+    public void init() {
+        // here we instantiate the SelectModes - set aside memory for that class
+        modes = new SelectModes(this);
+        // here we instantiate the GamepadTask - set aside memory for that class
+        gamepad1Task = new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1);
+        // begins monitoring the gamepad controls and generates gamepad events when the controls are pressed
+        addTask(gamepad1Task);
+
+    }
+
+    // the handleEvent method is called after the task is added and a control is pressed
+    @Override
+    public void handleEvent(RobotEvent e) {
+        GamepadTask.GamepadEvent event = (GamepadTask.GamepadEvent) e;
+
+        switch (event.kind) {
+            case BUTTON_X_DOWN:
+                // do whatever when button is pressed
+                break;
+            case RIGHT_TRIGGER_DOWN:
+                // do whatever when trigger is pressed
+                break;
+        }
+    }
+
+    @Override
+    public void start() {
+
+    }
 }
+
