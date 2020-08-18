e093109f2fbc1601f66abf6a4d473f23ac71a627
emilylhsieh <emilyhsieh@rocknrollrobots25.com>
2020-08-17 20:19:25 -0700

    Started ServoMode control

--- 2020/TestRig/ServoMode.java
+++ 2020/TestRig/ServoMode.java
@@ -2,6 +2,8 @@ package TestRig;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
+import team25core.GamepadTask;
+
 class ServoMode {
     public enum ServoPosition {
         SERVO_LOW,
@@ -9,15 +11,41 @@ class ServoMode {
         SERVO_HIGH,
     }
 
-    private Servo servo1;
-    private Servo servo2;
-    private Servo servo3;
-    private Servo servo4;
+    List<Servo> servoList;
 
     public void init() {
         // servo1 = hardwareMap.servo.get("servo1");
         // servo2 = hardwareMap.servo.get("servo2");
         // servo3 = hardwareMap.servo.get("servo3");
         // servo4 = hardwareMap.servo.get("servo4");
+
     }
+
+    protected void setUpGamepad1ForServoControl(GamepadTask.GamepadEvent event) {
+
+        // casting instance of myRobot(generic robot type) into TestRigDemo type
+        TestRigDemo demoInstance = (TestRigDemo) myRobot;
+
+        switch (event.kind) {
+            case RIGHT_TRIGGER_DOWN:
+                // drive servo
+                break;
+            case DPAD_UP_DOWN:
+                // select servo 1
+                break;
+            case DPAD_RIGHT_DOWN:
+                // select servo 2
+                break;
+            case DPAD_DOWN_DOWN:
+                // select servo 3
+                break;
+            case DPAD_LEFT_DOWN:
+                // select servo 4
+                break;
+            default:
+                // msgTelem.setValue("Use right trigger to select mode, use button X to exit mode selection");
+        }
+        return;
+    }
+
 }
